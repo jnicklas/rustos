fe4bc2fa972d7bb728eadeee55aea0dcba1016b4
Jonas Nicklas and Nicklas Ramhöj <dev+jonas+nicklas@elabs.se>
2015-11-13 14:23:20 +0100

    Add wrapper around VGA buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,23 @@
-#![feature(no_std, lang_items)]
+#![feature(no_std, lang_items, const_fn, unique, core_str_ext)]
 #![no_std]
+
+mod vga_buffer;
+
+use vga_buffer::{Writer, Color};
+
+use core::fmt::Write;
 
 extern crate rlibc;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let x = ["Hello", " ", "World", "!"];
+    let mut writer = Writer::new();
+    writer.write_byte(b'H');
+    writer.set_color(Color::LightBlue, Color::Black);
+    writer.write_str("ello! ");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
+
+    loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
